c9730661a69ad579b29e6f7d973e553d5d155a9c
Abel Kashoba <lebakashoba@hotmail.fr>
2024-11-05 19:52:33 +0100

    Finished the main index.js file. Which will export the web39 with function to update and add zone data

--- index.js
+++ index.js
@@ -58,10 +58,62 @@ export class Web39{
 		})
 	}
 
+	async add_zone_data({name,ttl=120,ip, serial=10293890}){
+		return new Promise((resolve,reject)=>{
+			let data = `zone=${this.#zone}&serial=${serial}&add=${JSON.stringify({
+				dname:`${name}.${this.#zone}.`, ttl, record_type:'A', line_index: null, data:[ip]
+			})}`,
+			req = https.request(`${BASE_URL}${this.#security_token}/${ZONE_EDIT_PATH}`,{
+				agent,
+				method:'POST',
+				headers:{
+					'Content-Type':'application/x-www-form-urlencoded',
+					'Cookie': this.#session_cookie,
+					'Content-Length': Buffer.byteLength(data)
+				}
+			},async (res)=>{
+				try{
+					let data = await get_response_data(res);
+
+					if(data.status){
+						resolve(true);
+					}
+					else{
+						if(data.errors){
+							let error = data.errors[0],
+							serial = error.match(/\d{10}/g)[0];
+
+							if(serial){
+								await this.add_zone_data({ name,ttl, ip, serial });
+							}
+							else{
+								console.error("No serial found in error",data.errors);
+								return reject(new Error("No serial found"));
+							}
+						}
+						else{
+							console.log("ODD DATA",data);
+							reject(new Error("Data couldn't be updated"));
+						}
+					}
+				}
+				catch(error){
+					reject(error);
+				}
+			});
+
+			console.log('DATA TO SEND',data);
+
+			req.on('error',reject);
+
+			req.end(data);
+		})
+	}
+
 	async update_zone_data({name,ttl=120,ip, line_index, serial=10293890}){
 		return new Promise((resolve,reject)=>{
 			let data = `zone=${this.#zone}&serial=${serial}&edit=${JSON.stringify({
-				dname:`${name}.${this.#zone}`, ttl, record_type:'A', line_index, data:[ip]
+				dname:`${name}.${this.#zone}.`, ttl, record_type:'A', line_index, data:[ip]
 			})}`,
 			req = https.request(`${BASE_URL}${this.#security_token}/${ZONE_EDIT_PATH}`,{
 				agent,
